79f0ea3d671c02dd7e8b5548a2a30274399238a9
MKurshakova <mariia.kurshakova@gmail.com>
2024-06-16 19:33:36 +0200

    fox fall

--- le-petit-prince-game/fox.h
+++ le-petit-prince-game/fox.h
@@ -5,30 +5,43 @@
 #include "map.h"
 
 struct Fox {
-    enum States {
-        REST=0, JUMP=1, RUN=2
-    };
+    enum States { REST=0, JUMP=1, RUN=2, FALL=3 };
 
     Fox(SDL_Renderer *renderer) :
             renderer(renderer),
             sprites{Animation(renderer, "assets/fox_idle.bmp",    128, 1.0, true ),
                     Animation(renderer, "assets/fox_jump.bmp",  128, 1.3, false ),
-                    Animation(renderer, "assets/fox_run.bmp",  128, 1.0, true )} {
+                    Animation(renderer, "assets/fox_run.bmp",  128, 1.0, true ),
+                    Animation(renderer, "assets/fox_fall.bmp",  128, 1.0, true )} {
     }
 
     void set_state(int s) {
         timestamp = Clock::now();
         state = s;
-        if (state==REST)
+        if (state!=JUMP && state!=RUN)
             vx = 0;
-        if (state==RUN)
+        else if (state==RUN)
             vx = backwards ? -150 : 150;
+        else if (state==JUMP) {
+            vy = jumpvy;
+            vx = backwards ? -jumpvx : jumpvx;
+        }
     }
 
     void handle_keyboard() {
         const Uint8 *kbstate = SDL_GetKeyboardState(NULL);
         if (state==RUN && !kbstate[SDL_SCANCODE_RIGHT] && !kbstate[SDL_SCANCODE_LEFT])
             set_state(REST);
+        if ((state==REST || state==RUN) && kbstate[SDL_SCANCODE_UP]) {
+            if (kbstate[SDL_SCANCODE_LEFT] || kbstate[SDL_SCANCODE_RIGHT]) {
+                jumpvx =  200; // long jump
+                jumpvy = -200;
+            } else {
+                jumpvx =   50; // high jump
+                jumpvy = -300;
+            }
+            set_state(JUMP);
+        }
         if (state==REST && (kbstate[SDL_SCANCODE_LEFT] || kbstate[SDL_SCANCODE_RIGHT])) {
             backwards = kbstate[SDL_SCANCODE_LEFT];
             set_state(RUN);
@@ -36,32 +49,27 @@ struct Fox {
     }
 
     void update_state(const double dt, const Map &map) {
-        double cand_x = x + dt*vx; // candidate coordinates prior to collision detection
+        if (state==JUMP && sprites[state].animation_ended(timestamp))
+            set_state(REST);
+        if (state!=JUMP && map.is_empty(x/map.tile_w, y/map.tile_h + 1))
+            set_state(FALL);   // put free falling sprite if no ground under the feet
 
-        if (map.is_empty(x/map.tile_w, y/map.tile_h + 1))
-            set_state(JUMP);   // put free falling sprite if no ground under the feet
-
-        double cand_y = y + dt*vy;
-        vy += dt*300; // gravity
-
-
-        if (!map.is_empty(cand_x/map.tile_w, y/map.tile_h)) { // horizontal collision detection
-            int snap = std::round(cand_x/map.tile_w)*map.tile_w; // snap the coorinate to the boundary of last free tile
-            cand_x = snap + (snap>cand_x ? 1 : -1);
+        x += dt*vx; // prior to collision detection
+        if (!map.is_empty(x/map.tile_w, y/map.tile_h)) { // horizontal collision detection
+            int snap = std::round(x/map.tile_w)*map.tile_w; // snap the coorinate to the boundary of last free tile
+            x = snap + (snap>x ? 1 : -1);
             vx = 0; // stop
         }
 
-        if (!map.is_empty(cand_x/map.tile_w, cand_y/map.tile_h)) { // vertical collision detection
-            if (state==JUMP)
-                set_state(REST);
+        y  += dt*vy;  // prior to collision detection
+        vy += dt*300; // gravity
+        if (!map.is_empty(x/map.tile_w, y/map.tile_h)) { // vertical collision detection
+            int snap = std::round(y/map.tile_h)*map.tile_h;   // snap the coorinate to the boundary of last free tile
+            y = snap + (snap>y ? 1 : -1);
             vy = 0; // stop
-            int snap = std::round(cand_y/map.tile_h)*map.tile_h; // snap the coorinate to the boundary of last free tile
-            cand_y = snap + (snap>cand_y ? 1 : -1);// be careful to snap to the left or to the right side of the free tile
+            if (state==JUMP || state==FALL)
+                set_state(REST);
         }
-
-
-        x = cand_x; // final coordinates post-collision detection
-        y = cand_y;
     }
 
 
@@ -71,17 +79,17 @@ struct Fox {
         SDL_RenderCopyEx(renderer, sprites[state].texture, &src, &dest, 0, nullptr, backwards ? SDL_FLIP_HORIZONTAL : SDL_FLIP_NONE);
     }
 
-    double x = 150, y = 200; // coordinates of the player
-    double vx = 150, vy = 200; //speed
-    bool backwards = false;  // left or right
+    double x = 150, y = 200; // coordinates of the character
+    double vx = 0, vy = 0;   // speed
+    bool backwards = false;  // facing left or right
+    double jumpvx = 0, jumpvy = 0; // will be used to differentiate high jump from a long jump
 
-    int state = REST;
+    int state = REST;         // current sprite
     TimeStamp timestamp = Clock::now();
+    SDL_Renderer *renderer;   // draw here
 
     const int sprite_w = 128; // size of the sprite on the screen
     const int sprite_h = 128;
-
-    SDL_Renderer *renderer;   // draw here
-    std::array<Animation,3> sprites; // sprite sequences to be drawn
+    const std::array<Animation,4> sprites; // sprite sequences to be drawn
 };
 #endif //LE_PETIT_PRINCE_GAME_FOX_H
